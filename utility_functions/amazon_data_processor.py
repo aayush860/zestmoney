0344e70d0df5c317da13e6297287f26a7a61d3ce
Aayush Bhargava <aayush.bhrgv@gmail.com>
2020-08-30 04:39:51 +0530

    chNGES

--- utility_functions/amazon_data_processor.py
+++ utility_functions/amazon_data_processor.py
@@ -5,7 +5,7 @@ class amazon_data_processor:
 
     def process(self):
         self.data = self.string.split('\n')
-        indexx = self.data.index('Apple iPhone XR (64GB) - Blue')
+        indexx = self.data.index('Apple iPhone 11 (64GB) - Black')
         value = int(self.data[indexx+2].split('₹')[1].replace(',', ''))
         return value
 
